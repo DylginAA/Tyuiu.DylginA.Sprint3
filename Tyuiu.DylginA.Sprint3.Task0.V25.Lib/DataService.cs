6d44e86763819884768fe4924ff828d7b8f555fe
Admin <Admin@HOME-PC>
2024-10-27 13:13:05 +0500

    Task0

--- Tyuiu.DylginA.Sprint3.Task0.V25.Lib/DataService.cs
+++ Tyuiu.DylginA.Sprint3.Task0.V25.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.DylginA.Sprint3.Task0.V25.Lib
 {
     public class DataService : ISprint3Task0V25
@@ -8,11 +9,11 @@ namespace Tyuiu.DylginA.Sprint3.Task0.V25.Lib
             double sumSeries = 0;
             int i;
 
-            for (i = startValue; i <= stopValue; i++)
+            for (i = 1; i <= 14; i++)
             {
-                sumSeries = Math.Pow(4.0 / (1 + Math.Pow(value, i)), i);
-            } 
-            return sumSeries;
+                sumSeries += Math.Pow(4 / (1 + Math.Pow(value, i)), i);
+            }
+            return Math.Round(sumSeries,3);
 
         }
     }
