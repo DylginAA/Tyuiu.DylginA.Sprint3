b3f6879b3b1c89d82bfc9ec132223579eff2c47c
Admin <Admin@HOME-PC>
2024-10-31 20:01:50 +0500

    Task7

---
+++ Tyuiu.DylginA.Sprint3.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.DylginA.Sprint3.Task7.V1.Lib
+{
+    public class DataService : ISprint3Task7V1
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+                int length = stopValue - startValue + 1;
+                double[] massResult = new double[length];
+
+                for (int x = startValue; x <= stopValue; x++)
+                {
+                    double result;
+                    if (x + 1.2 == 0)
+                    {
+                        result = 0;
+                    }
+                    else
+                    {
+                        result = Math.Round((Math.Sin(x) / (x + 1.2) + Math.Cos(x) * 7 * x - 2), 2);
+                    }
+                    massResult[x - startValue] = result;
+                    Console.WriteLine($"F({x}) = {result}");
+                }
+                return massResult;
+            }
+    }
+}
---
+++ Tyuiu.DylginA.Sprint3.Task7.V1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.DylginA.Sprint3.Task7.V1.Lib;
+namespace Tyuiu.DylginA.Sprint3.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах");
+            Console.WriteLine("* Задание #6");
+            Console.WriteLine("* Вариант #3");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:На отрезке, где x принимает значения от 13 до 19, вычислить сумму делителей больше 8");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Начальное значение диапозона = -5");
+            Console.WriteLine("Конечное  значение диапазона= 5");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.GetMassFunction(startValue, stopValue));
+        }
+    }
+}
