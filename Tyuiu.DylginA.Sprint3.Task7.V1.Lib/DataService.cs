a911f066c1ade34d81964f63adc98bcf649831b5
Admin <Admin@HOME-PC>
2024-10-31 20:03:17 +0500

    Task7

--- Tyuiu.DylginA.Sprint3.Task7.V1.Lib/DataService.cs
+++ Tyuiu.DylginA.Sprint3.Task7.V1.Lib/DataService.cs
@@ -5,24 +5,25 @@ namespace Tyuiu.DylginA.Sprint3.Task7.V1.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-                int length = stopValue - startValue + 1;
-                double[] massResult = new double[length];
+            double[] MassResult = new double[11];
 
-                for (int x = startValue; x <= stopValue; x++)
+            int index = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double result;
+                if (x + 1.2 == 0)
                 {
-                    double result;
-                    if (x + 1.2 == 0)
-                    {
-                        result = 0;
-                    }
-                    else
-                    {
-                        result = Math.Round((Math.Sin(x) / (x + 1.2) + Math.Cos(x) * 7 * x - 2), 2);
-                    }
-                    massResult[x - startValue] = result;
-                    Console.WriteLine($"F({x}) = {result}");
+                    result = 0;
                 }
-                return massResult;
+                else
+                {
+                    result = Math.Round((Math.Sin(x) / (x + 1.2) + Math.Cos(x) * 7 * x - 2), 2);
+                }
+                MassResult[index] = result;
+                Console.WriteLine($"F({x}) = {result}");
+                index++;
             }
+            return MassResult;
+        }
     }
 }
